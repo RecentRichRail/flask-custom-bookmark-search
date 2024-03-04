77d80d973c4cbfdd95ced66dbb9103cad03c6326
RecentRichRail <80782821+RecentRichRail@users.noreply.github.com>
2024-03-03 23:15:21 -0500

    Update app.run() parameters

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def redirect_command(command):
         return redirect(request.url.format(request.encoded_query))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='80', debug=True)
